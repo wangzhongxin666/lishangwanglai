c6bf6e73a2f2cbb4dfbb6afdc2657fe0d44b4b35
wangzhongxin666 <58760090+wangzhongxin666@users.noreply.github.com>
2020-02-25 20:51:31 +0800

    Add files via upload
    项目

---
+++ lishangwanglai/src/component/Manage/Manage .jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import './Manage.css'
+export default class Manage extends Component{
+constructor(){
+    super()
+}
+render(){
+    return(
+        <div>
+		理账
+		
+		</div>
+    )
+}
+}
+
